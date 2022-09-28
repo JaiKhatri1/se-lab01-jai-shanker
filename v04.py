b6a73a2af0db74a566ce9e1200687c2abda3c10c
DarshanDayal123 <darshan2020@namal.edu.pk>
2022-09-28 10:10:24 -0700

    changes from jai's repo

---
+++ v04.py
@@ -0,0 +1,12 @@
+#calculator in Python (v0.4)
+def add(num1,num2):
+   return num1+num2
+
+def sub(num1,num2):
+   return num1-num2
+
+def mul(num1,num2):
+   return num1+num2
+
+def div(num,num2):
+   return num1/num2
