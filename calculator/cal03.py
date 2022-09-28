449689f0445b704cdb2f8381fa08d46dc3bf356f
DarshanDayal123 <darshan2020@namal.edu.pk>
2022-09-28 09:58:00 -0700

    from jai repo

---
+++ calculator/cal03.py
@@ -0,0 +1,83 @@
+#calculator in python(v0.1)
+
+def add(num1,num2):
+    return num1+num2
+
+#main
+res = add(5,3)
+print(res)
+
+
+#calculator in python(v0.2)
+
+def add(num1,nun2):
+    return num1+num2
+def sub(num1,num2)
+   return num1-num2
+
+#main 
+res = add(5,3)
+print(res)
+print(sub(5,3))
+
+
+#calculator in Python (v0.3)
+def add(num1,num2):
+   return num1+num2
+
+def sub(num1,num2):
+   return num1-num2
+
+def mul(num1,num2):
+   return num1+num2
+
+#main 
+res =add(num1,num2)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+
+#calculator in Python (v0.4)
+def add(num1,num2):
+   return num1+num2
+
+def sub(num1,num2):
+   return num1-num2
+
+def mul(num1,num2):
+   return num1+num2
+
+def div(num,num2):
+   return num1/num2
+
+#main 
+res =add(num1,num2)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+
+#calculator in Python (v0.5)
+def add(num1,num2):
+   return num1+num2
+
+def sub(num1,num2):
+   return num1-num2
+
+def mul(num1,num2):
+   return num1+num2
+
+def div(num1,num2):
+   return num1/num2
+
+def sqroot(num1):
+   return num1**(0.5)
+
+
+#main 
+res =add(num1,num2)
+print(res)
+print(sub(5,3))
+print(mul(5,3))
+print(div(5,3))
+print(sqroot(9))
